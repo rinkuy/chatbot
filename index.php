0330d5ba53006b4b79feef8ba054355992c3c8b4
rinkuy <47396459+rinkuy@users.noreply.github.com>
2019-02-07 00:15:55 +0530

    changes

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php 
-
+echo 'hello'; die;
+$method=$_SERVER['REQUEST_METHOD'];
+if($method == "POST"){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);	
+	
+    $text = $json->result->parameters->text;
+	
+	switch($text){
+	 case 'hi':
+	  $speech = "Hi, Nice to meet you";
+	  break;
+	 case 'bye':
+	  $speech = "Bye, Good night";
+	  break;
+	 case 'anything':
+	  $speech = "Yes, You can type anything here. ";
+	  break;
+	 default:
+       $speech = "Sorry, I didnt get that. Please ask me something else"	 
+	    break;
+	}
+	
+	$response= new \stdClass();
+	$response->speech = "";
+	$response->displayText = "";
+	$response->source ="webhook";
+	echo json_encode($response);
+	
+  
+}
+ else{
+ echo "Method is not allowed"
+}
 ?>
